9eff56f6f4672b592a2bd6bb0b2dc43ac4c41332
Souvik Bhunia <143901095+souvik2002-bca@users.noreply.github.com>
2026-05-03 00:45:02 +0530

    Enable route protection for dashboard routes

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { getAdminDashboard, getTeacherDashboard, getStudentDashboard } = require('../controllers/dashboardController');
 const { protect, authorize } = require('../middleware/authMiddleware');
 
-//router.use(protect);
+router.use(protect);
 
 router.get('/admin', authorize('admin'), getAdminDashboard);
-router.get('/teacher', getTeacherDashboard);
+router.get('/teacher', authorize('teacher'), getTeacherDashboard);
 router.get('/student', authorize('student'), getStudentDashboard);
 
 module.exports = router;
