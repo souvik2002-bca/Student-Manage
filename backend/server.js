66a83be3d7e5599ba5fd4eb0149af935e922e952
Souvik Bhunia <143901095+souvik2002-bca@users.noreply.github.com>
2026-05-03 00:08:08 +0530

    h
    h

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://184.73.120.141', credentials: true }));
+app.use(cors({ origin: 'http://184.73.120.141', methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
